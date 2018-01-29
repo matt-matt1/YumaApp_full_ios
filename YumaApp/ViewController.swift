945e61fd16b4d2c86c976dd7f2e9f1773c179adb
Yuma Usa <yumausa@gmail.com>
2018-01-29 09:56:36 -0500

    Initial Commit

---
+++ YumaApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  YumaApp
+//
+//  Created by Yuma Usa on 2018-01-29.
+//  Copyright © 2018 Yuma Usa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
