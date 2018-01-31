087526994c4a374a98a9ff71c81396996771c59a
Yuma Usa <yumausa@gmail.com>
2018-01-31 13:13:13 -0500

    global colors; added pod R.swift

--- YumaApp/ViewController.swift
+++ YumaApp/ViewController.swift
@@ -8,13 +8,24 @@
 
 import UIKit
 
-class ViewController: UIViewController, UIPageViewControllerDataSource {
+class ViewController: UIViewController/*, UIPageViewControllerDataSource*/ {
 
+	let dummy_string = NSLocalizedString("str_key", comment: "str_value")
+	let welcomeMessage = NSLocalizedString("welcome.message", comment: "")
+	let welcomeName = String(format: NSLocalizedString("welcome.withName", comment: ""), locale: NSLocale.current, "Alice")
+	let progress = String(format: NSLocalizedString("copy.progress", comment: ""), locale: NSLocale.current, 4, 23)
+	
+	let YumaRed = UIColor(red: 0.66666666669999997, green: 0.0, blue: 0.094117647060000004, alpha: 1)
+	let YumaYel = UIColor(red: 0.99942404029999998, green: 0.98555368190000003, blue: 0.0, alpha: 1)
+
+	@IBOutlet weak var ICIcon: UILabel!
+	@IBOutlet weak var ICText: UILabel!
+	
 	var slides: [Slide] = []
 	let slide1 = Slide(id: 1, image: "home-slider-printers", 	caption1: "Laser Printers", 	caption2: "", 		target: "")
 	let slide2 = Slide(id: 2, image: "home-slider-cartridges", 	caption1: "Toner Cartridges", 	caption2: "that last", target: "")
 	let slide3 = Slide(id: 3, image: "home-slider-laptops", 	caption1: "Laptop Computers", 	caption2: "", 		target: "")
-	var images = ["home-slider-printers", "home-slider-cartridges", "home-slider-laptops"]
+//	var images = ["home-slider-printers", "home-slider-cartridges", "home-slider-laptops"]
 	var pageViewController: UIPageViewController?
 	var currentIndex: Int = 0
 	
@@ -24,34 +35,37 @@ class ViewController: UIViewController, UIPageViewControllerDataSource {
 		slides.append(slide1)
 		slides.append(slide2)
 		slides.append(slide3)
-
+		
+/*
 		createPageViewController();
 		setupPageControl();
 		
 		Timer.scheduledTimer(timeInterval: 3, target: self, selector: #selector(self.next(_:)), userInfo: nil, repeats: true)
+*/
 	}
-	
+/*
 	@objc func next(_ timer: Timer) {
 		currentIndex += 1
-		if currentIndex == images.count-1 {
+		//if currentIndex == images.count-1 {
+		if currentIndex == slides.count-1 {
 			currentIndex = 0
 		}
 		pageViewController?.goToNextPage()
 	}
-
+*/
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
-
+/*
 	func createPageViewController() {
 		
 		let pageController = self.storyboard?.instantiateViewController(withIdentifier: "PageController") as! UIPageViewController
 		
 		pageController.dataSource = self
 		
-		//if slides.count > 0 {
-		if images.count > 0 {
+		if slides.count > 0 {
+		//if images.count > 0 {
 			
 			let firstController = getItemController(0)!
 			let startingViewControllers = [firstController]
@@ -81,8 +95,8 @@ class ViewController: UIViewController, UIPageViewControllerDataSource {
 	
 	func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
 		let itemController = viewController as! ItemViewController
-		//if itemController.itemIndex+1 < slides.count {
-		if itemController.itemIndex+1 < images.count {
+		if itemController.itemIndex+1 < slides.count {
+		//if itemController.itemIndex+1 < images.count {
 			return getItemController(itemController.itemIndex+1)
 		}
 		return nil
@@ -93,8 +107,8 @@ class ViewController: UIViewController, UIPageViewControllerDataSource {
 	}
 	
 	func presentationCount(for pageViewController: UIPageViewController) -> Int {
-		//return slides.count
-		return images.count
+		return slides.count
+		//return images.count
 	}
 	
 	func presentationIndex(for pageViewController: UIPageViewController) -> Int {
@@ -109,18 +123,19 @@ class ViewController: UIViewController, UIPageViewControllerDataSource {
 	}
 	
 	func getItemController(_ itemIndex: Int) -> ItemViewController? {
-		//if itemIndex < slides.count {
-		if itemIndex < images.count {
+		if itemIndex < slides.count {
+		//if itemIndex < images.count {
 			let pageItemController = self.storyboard?.instantiateViewController(withIdentifier: "ItemController") as! ItemViewController
-			pageItemController.itemIndex = itemIndex
-			//pageItemController.imageName = slides[itemIndex].image
-			pageItemController.imageName = images[itemIndex]
+			//pageItemController.itemIndex = itemIndex
+			pageItemController.imageName = slides[itemIndex].image
+			//pageItemController.imageName = images[itemIndex]
 			return pageItemController
 		}
 		return nil
 	}
+*/
 }
-
+/*
 extension UIPageViewController {
 	func goToNextPage(animated: Bool = true, completion: ((Bool) -> Void)? = nil) {
 		if let currentViewController = viewControllers?[0] {
@@ -130,3 +145,4 @@ extension UIPageViewController {
 		}
 	}
 }
+*/
